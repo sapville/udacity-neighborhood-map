1e9f5b48da510255d23db5a822ac8539aef2b5a3
Evgenii Fedoseev <abap@sapville.com>
2017-12-29 16:20:17 +0300

    chore: initial setup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-function main() { //eslint-disable-line no-unused-vars
-  console.log('invoked');
+class App {
+  constructor (param) {
+    this.param = param;
+  }
+
+  inColor (elem) {
+    $(elem).toggleClass(this.param);
+  }
+
+}
+
+function main () { //eslint-disable-line no-unused-vars
+  const app = new App('blue');
+  $('h1').click(() => {app.inColor('h1');}) ;
 }
