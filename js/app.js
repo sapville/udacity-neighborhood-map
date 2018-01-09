13b5f8968ec86178184de4f2c14a427811b1b417
Evgenii Fedoseev <abap@sapville.com>
2018-01-09 16:12:22 +0300

    feat: load google map

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
+// import ko from 'js/knockout-3.4.2';
+
+/*
+global
+ko
+*/
+
 class App {
-  constructor (param) {
-    this.param = param;
+  constructor (mapLoadStatus, errorCode) {
+    this.alertVisible = ko.observable(false);
+    this.alertText = ko.observable('');
+    if (!mapLoadStatus) {
+      this.alertVisible(true);
+      this.alertText(`Map load error with code ${errorCode}`);
+    }
   }
 
-  inColor (elem) {
-    $(elem).toggleClass(this.param);
+  static mapStatus () {
+    return {
+      MAP_LOAD_FAILURE: false,
+      MAP_LOAD_SUCCESS: true
+    };
   }
 
 }
 
-function main () { //eslint-disable-line no-unused-vars
-  const app = new App('blue');
-  $('h1').click(() => {app.inColor('h1');}) ;
+function init () { //eslint-disable-line no-unused-vars
+}
+
+function mapError () { //eslint-disable-line no-unused-vars
+  const app = new App(App.mapStatus().MAP_LOAD_FAILURE);
+  ko.applyBindings(app);
 }
