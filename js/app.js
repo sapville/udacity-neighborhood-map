3fa23eecd7c2dda4224bcaf87ac0a1fedf216021
Evgenii Fedoseev <abap@sapville.com>
2018-01-09 23:29:37 +0300

    feat: error handling prototype

--- js/app.js
+++ js/app.js
@@ -5,6 +5,16 @@ global
 ko
 */
 
+(function () {
+  const script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBOPYXNAFOVDoakdOLtdqpI71E3erG-RkQ&callback=init&v=3';
+  script.defer = true;
+  document.head.appendChild(script);
+  script.onerror = function (event) {
+    console.log('onerror', event);
+  };
+})();
+
 class App {
   constructor (mapLoadStatus, errorCode) {
     this.alertVisible = ko.observable(false);
@@ -25,9 +35,13 @@ class App {
 }
 
 function init () { //eslint-disable-line no-unused-vars
+  console.log('init');
 }
 
-function mapError () { //eslint-disable-line no-unused-vars
+function mapError (message, source, lineno, colno, error) { //eslint-disable-line no-unused-vars
+  console.log(message);
   const app = new App(App.mapStatus().MAP_LOAD_FAILURE);
   ko.applyBindings(app);
 }
+
+function gm_authFailure (error) { console.log('Auth error');}
